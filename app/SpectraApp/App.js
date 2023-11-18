cfb46346c43567df46ff130a2ee7e42fea42b68b
mithunsubhash <mkamalapriya3@gatech.edu>
2023-11-18 12:32:29 -0500

    Added navigation

--- app/SpectraApp/App.js
+++ app/SpectraApp/App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import { NavigationContainer } from "@react-navigation/native";
+import { PaperProvider } from 'react-native-paper';
+import Settings from "./screens/Settings";
+import Welcome from "./screens/Welcome";
+import Video from "./screens/Video";
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome" >
+          <Stack.Screen name="Settings" component={Settings}  />
+          <Stack.Screen name="Video" component={Video} />
+          <Stack.Screen name="Welcome" component={Welcome} />
+        </Stack.Navigator>
+      </NavigationContainer> 
+    </PaperProvider>
   );
 }
 
